535e8ded1b3ca2e07f2d03c4df2fddc6a0ef1c6a
Md Shayon <mdshayon0@gmail.com>
2019-03-23 02:32:03 +0600

    get all information in console about user from google

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -9,9 +9,10 @@ passport.use(
         callbackURL: '/auth/google/redirect', // FOR THIS TIME WE DON'T NEED TO SET LOCALHOST:PORT IT AUTOMITICALLY KNOWS WHERE IT IS
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
-    }, ()=>{
+    }, ( accessToken, refreshToken, profile, done)=>{
         //PASSPORT CALLBACK FUNCTION
         // WHEN WE ALLOW WITH GOOGLE THIS FUNCTION FILL BE CALLED
         console.log('Passport callback function');
+        console.log(profile);
     })
 )
